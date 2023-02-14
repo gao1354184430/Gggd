273df126904b5ee85e42da17a13973e055cbe015
gao1354184430 <50170219+gao1354184430@users.noreply.github.com>
2023-02-14 16:20:09 +0800

    Update oneclick-install.sh

--- test/test5/noip/oneclick-install.sh
+++ test/test5/noip/oneclick-install.sh
@@ -1,89 +1,83 @@
 #!/bin/bash
 
-# 获取命令行传递的ddns域名参数
-ddns_domain="$1"
+# 获取命令行参数
+noip_account=$1
+noip_password=$2
+ddns_domain=$3
 
-# 定义noip账号和密码
-noip_username="gao1354184430@gmail.com"
-noip_password="fV$XQZz8PTt@B4."
-
-# 检查操作系统类型，使用不同的安装命令和服务管理工具
-if [[ $(cat /etc/os-release) == *"CentOS"* ]]; then
-    # 安装noip客户端
-    sudo yum install -y wget gcc make
-    wget https://www.noip.com/client/linux/noip-duc-linux.tar.gz -O /tmp/noip-duc-linux.tar.gz
-    tar xf /tmp/noip-duc-linux.tar.gz -C /tmp/
-    cd /tmp/noip-2.1.9-1/
-    make
-    sudo make install
-
-    # 创建systemd服务文件
-    sudo tee /etc/systemd/system/noip2.service > /dev/null <<EOF
-[Unit]
-Description=noip2 Dynamic DNS Update Client
-After=network.target
-
-[Service]
-Type=forking
-ExecStart=/usr/local/bin/noip2
-Restart=always
-User=root
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    # 配置noip客户端
-    sudo sed -i "s/^\s*#*username=.*/username=${noip_username}/" /usr/local/etc/no-ip2.conf
-    sudo sed -i "s/^\s*#*password=.*/password=${noip_password}/" /usr/local/etc/no-ip2.conf
-    sudo sed -i "s/^\s*#*host=.*/host=${ddns_domain}/" /usr/local/etc/no-ip2.conf
-
-    # 重新加载systemd配置
-    sudo systemctl daemon-reload
-
-    # 启动noip2服务并设置自启动
-    sudo systemctl start noip2.service
-    sudo systemctl enable noip2.service
-
-elif [[ $(cat /etc/os-release) == *"Debian"* ]] || [[ $(cat /etc/os-release) == *"Ubuntu"* ]]; then
-    # 安装noip客户端
-    sudo apt-get update
-    sudo apt-get install -y build-essential
-    wget https://www.noip.com/client/linux/noip-duc-linux.tar.gz -O /tmp/noip-duc-linux.tar.gz
-    tar xf /tmp/noip-duc-linux.tar.gz -C /tmp/
-    cd /tmp/noip-2.1.9-1/
-    make
-    sudo make install
-
-    # 创建systemd服务文件
-    sudo tee /etc/systemd/system/noip2.service > /dev/null <<EOF
-[Unit]
-Description=noip2 Dynamic DNS Update Client
-After=network.target
-
-[Service]
-Type=forking
-ExecStart=/usr/local/bin/noip2
-Restart=always
-User=root
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    # 配置noip客户端
-    sudo sed -i "s/^\s*#*username=.*/username=${noip_username}/" /usr/local/etc/no-ip2.conf
-    sudo sed -i "s/^\s*#*password=.*/password=${noip_password}/" /usr/local/etc/no-ip2.conf
-    sudo sed -i "s/^\s*#*host=.*/host=${ddns_domain}/" /usr/local/etc/no-ip2.conf
-
-    # 重新加载systemd配置
-   
-sudo systemctl daemon-reload
-# 启动noip2服务并设置自启动
-sudo systemctl start noip2.service
-sudo systemctl enable noip2.service
+# 检查操作系统版本
+if [[ -e /etc/os-release ]]; then
+    . /etc/os-release
+    os=$ID
+    version=$VERSION_ID
 else
-echo "不支持的操作系统类型"
-exit 1
+    echo "Error: This script only supports CentOS 7, Debian and Ubuntu."
+    exit 1
 fi
-echo "noip ddns服务已部署成功！"
+
+# 安装依赖项
+case $os in
+    centos)
+        if [[ $version == "7" ]]; then
+            yum -y install wget make gcc openssl-devel
+        else
+            echo "Error: This script only supports CentOS 7."
+            exit 1
+        fi
+        ;;
+    debian|ubuntu)
+        apt-get update
+        apt-get -y install wget make gcc libssl-dev
+        ;;
+    *)
+        echo "Error: This script only supports CentOS 7, Debian and Ubuntu."
+        exit 1
+        ;;
+esac
+
+# 安装noip客户端
+cd /usr/local/src/
+wget https://www.noip.com/client/linux/noip-duc-linux.tar.gz
+tar xf noip-duc-linux.tar.gz
+cd noip-2.1.9-1/
+make
+make install
+
+# 创建noip配置文件
+cat <<EOF >/usr/local/etc/no-ip2.conf
+# Configuration file for noip2.
+# /usr/local/etc/no-ip2.conf
+# 
+# Updated: Sat Feb 13 00:00:12 PST 2021
+#
+# Use your account and password to login
+
+#日志文件位置
+#logfile=/tmp/noip.log
+
+#设置向no-ip注册的账号和密码
+username=${noip_account}
+password=${noip_password}
+
+#设置要DDNS更新的域名
+use=web, web=${ddns_domain}
+EOF
+
+# 创建systemd服务文件
+cat <<EOF >/etc/systemd/system/noip2.service
+[Unit]
+Description=No-IP Dynamic DNS Update Client
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/noip2
+Restart=always
+Type=forking
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 启动noip2服务
+systemctl enable noip2.service
+systemctl start noip2.service
